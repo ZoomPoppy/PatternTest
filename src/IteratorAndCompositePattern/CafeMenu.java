b81535663c26ba2a305366ffed3ec51316b125f3
ZoomPoppy <514027735@qq.com>
2015-04-27 14:11:41 +0800

    add cafemenu

---
+++ src/IteratorAndCompositePattern/CafeMenu.java
@@ -0,0 +1,19 @@
+package IteratorAndCompositePattern;
+
+import java.util.Hashtable;
+
+/**
+ * Created by zz on 2015/4/27.
+ */
+public class CafeMenu {
+    Hashtable menuItems = new Hashtable();
+     public CafeMenu(){
+         addItem("veggie Burger and air fries","veggie burger on a whole wheat bun ,lettuce ,tomato ,and fries",true,3.99);
+         addItem("Soup of the day","a cup of the soup of the dau,with a side salad",false,3.69);
+         addItem("Burrito","A large burrito ,with whole pinto beans ,salsa ,guacamole",true,4.28);
+     }
+    public void addItem(String name,String description,boolean vegetarian,double price){
+        MenuItem menuItem = new MenuItem(name,description,vegetarian,price);
+        menuItems.put(menuItem.getName(),menuItem);
+    }
+}
